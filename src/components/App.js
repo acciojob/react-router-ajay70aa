9d884f0f9e5f081e01f1b9c432ca604ea6b4ffef
Ajay70aa <ajay70aa@gmail.com>
2025-05-24 16:35:27 +0530

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
-
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom/cjs/react-router-dom.min";
 import React from "react";
 import './../styles/App.css';
+import Homepage from "./Homepage";
+import AboutPage from "./AboutPage";
 
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+      <Router>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Switch>
+          <Route exact path="/" component={Homepage}/>
+          <Route path="/about" component={AboutPage}/>
+        </Switch>
+      </Router>
     </div>
   )
 }
---
+++ src/components/Homepage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Homepage = () => {
+  return (
+    <div>
+      <h1>Welcome to my website!</h1>
+    </div>
+  )
+}
+
+export default Homepage
